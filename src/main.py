20996326a4ba994da558e92f276b76d9bc56268a
Pedro Serrano Faria <pedroserrano2@gmail.com>
2025-08-06 21:03:45 -0300

    feat: set up API server with Uvicorn and APIBuilder

--- src/main.py
+++ src/main.py
@@ -1,6 +1,11 @@
-def main():
-    print("Hello from project!")
+import uvicorn
 
+from src.infra.api import APIBuilder
+from src.settings import cfg
+
+apis = APIBuilder(cfg)
+apis.build_stack()
+app = apis.app
 
 if __name__ == "__main__":
-    main()
+    uvicorn.run("main:app", host="0.0.0.0", port=8003, access_log=True, reload=True)
